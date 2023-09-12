0ff387e7a40b21c68bf6a48b70be0ede0f8efb8a
Angel Gamis <agamis@rrc.ca>
2023-09-12 11:17:03 -0500

    Added a few of Dylan's Schmupp code files. Created a simple player object with a goose art asset. Then I added a self programmed movement script. I used Dylan's Player shooter script to get some sort of feel with shooting. Then I made a basic NPC using a self programmed script which allows the NPC to follow the player. I also made a prefab for the projectile, and used Dylan's Simple Force script on it.

---
+++ Assets/Scripts/NPCDemo.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class NPCDemo : MonoBehaviour
+{
+    public GameObject player;
+    private Vector2 playerPosition;
+    private Vector2 thisPosition;
+
+    private Vector2 vectorBetween;
+    private Vector2 normilizedVectorBetween;
+
+    public float speed = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wandering();
+
+        // Raycast, Detect Player
+    }
+
+    public void Wandering()
+    {
+        // Grabbing players current position
+        playerPosition = (Vector2)player.transform.position;
+
+        // NPCs position to vector2
+        thisPosition = (Vector2)transform.position;
+
+        // Finding the direction and magnitude between NPC and target player
+        vectorBetween = playerPosition - thisPosition;
+
+        // Normalize vector and multiply by a standardized magnitude
+        normilizedVectorBetween = vectorBetween.normalized;
+
+        // Change position of NPC to players position
+        transform.Translate(normilizedVectorBetween * Time.deltaTime * speed);
+
+        Debug.DrawRay(transform.position, normilizedVectorBetween);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("** Inputs **")]
+    public float inputx;
+    public float inputy;
+
+    public Vector2 inputs;
+
+    [Header("Stats")]
+    public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input values from keyboard
+        inputx = Input.GetAxis("Horizontal");
+        inputy = Input.GetAxis("Vertical");
+
+        // Input vector
+        inputs = new Vector2(inputx, inputy);
+
+        // Standardizing magnitude
+        float mag = Mathf.Min(inputs.magnitude, 1f);
+
+        // Make player move with normalized input vector, move per second, setting a speed, standardizing magnitude
+        transform.Translate(inputs.normalized * Time.deltaTime * speed * mag);
+
+    }
+}
