3a00e5a591b9bd7a5c9fd1492e140673f514e074
Angel Gamis <agamis@rrc.ca>
2023-09-14 15:44:19 -0500

    Made a second spawner script changing it to work on the Y axis to make all different side spawners. Also made the duck npcs get player object on spawn, so it knows where to go.

--- Assets/Scripts/NPCDemo.cs
+++ Assets/Scripts/NPCDemo.cs
@@ -18,7 +18,7 @@ public class NPCDemo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
