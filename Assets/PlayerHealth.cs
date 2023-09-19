b53271f9d049ab777b293144e062208144e37611
Angel Gamis <agamis@rrc.ca>
2023-09-19 10:44:27 -0500

    Made a health system for player
    Changed lives to be game objects instead of canvas UI to be able to control them more. I worked on player health script. I made a switch case that referenced the number of lifes the player has. If it has a certain amount change the active player life game objects. I also made a collision detection that will check if the goose (player) collided with a enemy then change its position to far away. While also invoking the death cases in a bit of a delay.

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,36 +1,87 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [Header ("** Positions **")]
-    private Transform playerTransform;
-    public Vector2 respawnPoint;
-    public Vector2 deathPoint;
+	[Header("** Positions **")]
+	public Transform playerTransform;
+	public Vector2 respawnPoint;
+	public Vector2 deathPoint = new Vector2(-15, -15);
+
+	[Header ("** References **")]
+	public GameObject extraLifePrefab;
+	private PlayerMovementPhysics playerMovement;
+
+	[Header ("** Stats **")]
+	public int livesCount = 3;
+	public float respawnDelay = 5f;
+	public int extraLives = 0;
+	public int maxLives = 5;
+
+	[Header("** Player Lives **")]
+	public GameObject playerLife1;
+    public GameObject playerLife2;
+    public GameObject playerLife3;
 
     // Start is called before the first frame update
     void Start()
-    {
-        playerTransform = transform;
-    }
+	{
+		playerTransform = transform;
+		playerMovement = GetComponent<PlayerMovementPhysics>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		switch (livesCount)
+		{
+			case 3:
+				playerLife3.SetActive(true);
+                playerLife2.SetActive(true);
+                playerLife1.SetActive(true);
+                break;
+			case 2:
+				playerLife3.SetActive(false);
+                playerLife2.SetActive(true);
+                playerLife1.SetActive(true);
+                break;
+			case 1:
+				playerLife3.SetActive (false);
+                playerLife2.SetActive(false);
+                playerLife1.SetActive(false);
+                break;
+		}
 
-    void PlayerDied()
-    {
+	}
 
-    }
+	void PlayerDied()
+	{
+		// Lower Lives
+		livesCount -= 1;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+		// Respawn in respawn position, if player still has lives
+		if (livesCount > 0)
+		{
+			playerTransform.position = respawnPoint;
+		}
+	}
 
-        // Move player out of screen.
-        // Delay
-        // Lower Lives & Respawn Player Function
-    }
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// Check if the collision was with an NPC
+		if (collision.gameObject.tag == "Enemy")
+		{
+			// Move player out of screen, to death position
+			playerTransform.position = deathPoint;
+			Debug.Log("Hit a duck");
+
+			// Delay respawn
+			Invoke("PlayerDied", respawnDelay);
+		}
+
+	}
 }
